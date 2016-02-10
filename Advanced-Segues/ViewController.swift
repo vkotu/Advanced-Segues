ee1f273da48b54d2c00f753f47cac25f9b6d2d63
venkat <vikkii89@gmail.com>
2016-02-10 15:35:50 -0800

    advanced segues

--- Advanced-Segues/ViewController.swift
+++ Advanced-Segues/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
+var rowCounter:Int = 0
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print(rowCounter)
     }
 
     override func didReceiveMemoryWarning() {
